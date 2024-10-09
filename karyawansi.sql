4000b843c1ccff0bc9c8600591e6a79ddd26b247
SHM69 <95047685+s4mu3l69@users.noreply.github.com>
2024-10-10 00:02:17 +0700

    Add files via upload

---
+++ karyawansi.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 24 Jul 2023 pada 15.57
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `karyawansi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_absen`
+--
+
+CREATE TABLE `tb_absen` (
+  `id` int(11) NOT NULL,
+  `id_karyawan` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `waktu_masuk` varchar(255) DEFAULT NULL,
+  `keterangan2` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_daftar`
+--
+
+CREATE TABLE `tb_daftar` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_daftar`
+--
+
+INSERT INTO `tb_daftar` (`id`, `username`, `password`) VALUES
+(2, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_jabatan`
+--
+
+CREATE TABLE `tb_jabatan` (
+  `id` int(11) NOT NULL,
+  `jabatan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tmp_tgl_lahir` varchar(255) NOT NULL,
+  `jenkel` varchar(255) NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_tel` varchar(18) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`id_karyawan`, `username`, `password`, `nama`, `tmp_tgl_lahir`, `jenkel`, `agama`, `alamat`, `no_tel`, `jabatan`, `foto`) VALUES
+(9, 'Abdul', 'd41d8cd98f00b204e9800998ecf8427e', 'Abdul Muhlisin Sudirman', 'Klaten / 19-09-1994', 'Laki-laki', 'Islam', '', '0895635721923', 'CEO', '21092020072509employee1.png'),
+(123, 'saddam ', '202cb962ac59075b964b07152d234b70', 'saddam', 'jakarta ', 'Laki-laki', 'Islam', 'jl hj saikin rt8/8 No 48 Kebayoran Lama Pondok Pinang Jakarta Selatan', '085694936935', 'Operator', '13072023074840webcam-toy-photo1.jpg'),
+(192, 'sarah', '9e9d7a08e048e9d604b79460b54969c3', 'Sarah Mutia', 'Cianjur / 10-12-1992', 'Perempuan', 'Islam', '', '08128384848', 'CEO', '10092020025112employee3.png'),
+(123411111, 'ihsan', 'd41d8cd98f00b204e9800998ecf8427e', 'ihsan deui', 'bandung', 'Laki-laki', 'Islam', 'jalan bandung', '09876544456', 'CEO', '19012023163345puskesmas2.png'),
+(201957020, 'liska', 'd41d8cd98f00b204e9800998ecf8427e', 'Liska Ismawati', 'Brebes', 'Perempuan', 'Islam', 'Ketanggungan', '12345', 'CEO', '15042022195436WhatsApp Image 2022-03-22 at 14.39.4'),
+(999999999, 'user', 'd41d8cd98f00b204e9800998ecf8427e', 'Liska', 'Jakarta / 10-01-1990', 'Laki-laki', 'Islam', '', '0895628383333', 'CEO', '15042022214855Fevicon.png');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_keterangan`
+--
+
+CREATE TABLE `tb_keterangan` (
+  `id` int(11) NOT NULL,
+  `id_karyawan` varchar(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `alasan` text NOT NULL,
+  `waktu` varchar(255) NOT NULL,
+  `bukti` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_daftar`
+--
+ALTER TABLE `tb_daftar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_jabatan`
+--
+ALTER TABLE `tb_jabatan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indeks untuk tabel `tb_keterangan`
+--
+ALTER TABLE `tb_keterangan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=192;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_daftar`
+--
+ALTER TABLE `tb_daftar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_jabatan`
+--
+ALTER TABLE `tb_jabatan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_keterangan`
+--
+ALTER TABLE `tb_keterangan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
